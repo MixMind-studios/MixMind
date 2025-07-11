56642d5922e22a6261559d4dd761d87d03b3e3f6
MixMind-studios <pohep@hotmail.com>
2025-07-10 22:00:43 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+// Replace with your actual backend URL when deploying
+export const BACKEND_URL = 'https://48a6f75e-06be-4a8a-853c-bf8815d34332.preview.emergentagent.com';
+export const API_BASE_URL = `${BACKEND_URL}/api`;
